ca12609bfd0c33f04117f444c4de5fa923f4bf32
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-23 17:11:39 +0000

    Configurando o menu mobile

--- components/js/script.js
+++ components/js/script.js
@@ -24,3 +24,35 @@ window.onscroll = function() {
     btnTopo.style.display = "none";
   }
 };
+
+
+const btnHamburguer = document.querySelector('.mobile-menu');
+const activeClass = 'active';
+
+const navbar = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu-item');
+const header = document.querySelector('header');
+
+btnHamburguer.addEventListener('click', function () {
+  if (this.classList.contains(activeClass)) {
+    this.classList.remove(activeClass);
+    
+    navbar.classList.remove(activeClass);
+    header.classList.remove(activeClass);
+  } else {
+    this.classList.add(activeClass);
+
+    navbar.classList.add(activeClass);
+    header.classList.add(activeClass);
+  }
+
+  for (let i in menuItems) {
+    menuItems[i].addEventListener('click', function () {
+      btnHamburguer.classList.remove(activeClass);
+    
+      navbar.classList.remove(activeClass);
+      header.classList.remove(activeClass);
+    })
+  }
+
+});
